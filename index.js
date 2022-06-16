b3133ddad2169b2170cae68ea0aaebcd74e75454
Ben Kiboma <omayioben@gmail.com>
2022-06-16 13:44:00 +0300

    Testing API

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-/*console.log(fetch('https://reqres.in/api/users'))
-  .then(res => {
-    if(res.ok) {
-      console.log("Sucess")
-    } else {
-      console.log("Failed")
-    }
-  })
-//    res.json())
-  //.then(data => console.log(data))*/
-
 function getGithubOrgs(url) {
   return fetch(url).then((response) => response.json());
 }
 
-getGithubOrgs("https://reqres.in/api/users").then((res) =>
-  console.log(res)
+getGithubOrgs("https://reqres.in/api/users")
+  .then((data) =>
+  console.log(data)
 );
