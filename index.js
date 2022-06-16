cdfb352cee6e0ac2f43dfb721c2ed083c588455a
Ben Kiboma <omayioben@gmail.com>
2022-06-16 14:51:40 +0300

    Testing API

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const get_url = 'https://api.wheretheiss.at/v1/satellites/25544'
 async function getData () {
   const response = await fetch(get_url);
   const data = await response.json();
-  console.log(data)
+  //console.log(data)
+  document.getElementById("lat").textContent=data.latitude;
+  document.getElementById("lon").textContent=data.longitude;
+  document.getElementById("vis").textContent=data.visibility;
 }
 getData();
